f86e9dfd218b2c0fdd36ddbf4466b9aeb77f001d
sumit177-k <royalryl1245@gmail.com>
2026-06-04 23:35:53 +0530

    Create Subarray sum equals k .cpp

---
+++ Subarray sum equals k .cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+     unordered_map<int,int>mpp;
+     int count=0;
+     int sum=0;
+     mpp[0]=1;
+     for(int i=0;i<nums.size();i++){
+        sum=sum+nums[i];
+        if(mpp.find(sum-k)!=mpp.end()){
+            count=count+mpp[sum-k];
+        }
+        mpp[sum]++;
+     }
+     return count;
+    }
+};
